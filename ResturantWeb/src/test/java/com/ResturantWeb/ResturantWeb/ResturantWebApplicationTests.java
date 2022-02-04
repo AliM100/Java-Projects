6096d232084da4c288efe62f2fec4359eb32008d
A <A@DESKTOP-REE672I>
2022-02-04 15:23:52 +0200

    Creation of the Project

---
+++ ResturantWeb/src/test/java/com/ResturantWeb/ResturantWeb/ResturantWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ResturantWeb.ResturantWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResturantWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
